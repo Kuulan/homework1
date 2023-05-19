80d523ec40fea1d68f319cff305f9928c96d8f6c
Kristina <zazaelka@gmail.com>
2023-05-19 20:22:33 +0100

    07_Intro_to_OOP

---
+++ src/homework7/Cat.java
@@ -0,0 +1,12 @@
+package homework7;
+
+public class Cat extends Pet {
+
+    public Cat(String color) {
+        super(color);
+    }
+
+    public void sayMeow(){
+        System.out.println("Мяу-Мяу");
+    }
+}
---
+++ src/homework7/Dog.java
@@ -0,0 +1,12 @@
+package homework7;
+
+public class Dog extends Pet {
+
+    public Dog(String color) {
+        super(color);
+    }
+
+    public void sayBark(){
+        System.out.println("Гав-гав");
+    }
+}
